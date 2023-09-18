28a31d6042d817b4c9c4354635876fdb23fdd59f
Nilay Savant <nilayofficial@gmail.com>
2023-09-18 22:00:54 +0530

    Minor cleanup

--- crates/mesh_geometry_utils/src/lib.rs
+++ crates/mesh_geometry_utils/src/lib.rs
@@ -1,6 +1,3 @@
-// For Geo crate we need to extend the recursion limit.
-#![recursion_limit = "256"]
-
 /// Data structures use to store, represent and generate mesh geometry.
 pub mod data_structures;
 /// Mesh building and generation utilities.
