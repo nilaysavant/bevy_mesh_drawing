de9d43c481b027a10c078db2dfab263a4942b20d
Nilay Savant <nilayofficial@gmail.com>
2023-09-26 23:35:57 +0530

    Add keybind settings to plugin to allow configuring the mode change keys for now. Will add others next.

--- crates/mesh_drawing/src/resources/plugin.rs
+++ crates/mesh_drawing/src/resources/plugin.rs
@@ -11,12 +11,14 @@ pub enum PluginState {
 }
 
 /// Settings to configure the plugin.
-#[derive(Debug, Clone, Resource)]
+#[derive(Debug, Clone, Copy, Resource)]
 pub struct PluginSettings {
     /// Enable insert vertex functionality in edit mode.
     pub is_edit_mode_insert_vertex_enabled: bool,
     /// Enable remove vertex functionality in edit mode.
     pub is_edit_mode_remove_vertex_enabled: bool,
+    /// Key binds for the plugin.
+    pub key_binds: PluginKeyBinds,
 }
 
 impl Default for PluginSettings {
@@ -24,6 +26,25 @@ impl Default for PluginSettings {
         Self {
             is_edit_mode_insert_vertex_enabled: true,
             is_edit_mode_remove_vertex_enabled: true,
+            key_binds: PluginKeyBinds::default(),
+        }
+    }
+}
+
+/// Key binds for the plugin.
+#[derive(Debug, Clone, Copy, Resource)]
+pub struct PluginKeyBinds {
+    /// [`KeyCode`] used to switch to [`EditMode`](`super::DrawingMode::EditMode`)
+    pub edit_mode_switch_key: KeyCode,
+    /// [`KeyCode`] to switch to [`CreateMode`](`super::DrawingMode::CreateMode`)
+    pub create_mode_switch_key: KeyCode,
+}
+
+impl Default for PluginKeyBinds {
+    fn default() -> Self {
+        Self {
+            edit_mode_switch_key: KeyCode::Key1,
+            create_mode_switch_key: KeyCode::Key2,
         }
     }
 }
--- crates/mesh_drawing/src/systems/drawing_mode.rs
+++ crates/mesh_drawing/src/systems/drawing_mode.rs
@@ -1,16 +1,21 @@
 use bevy::prelude::*;
 
-use crate::events::{create_mode::CreateModeEvent, edit_mode::EditModeEvent};
+use crate::{
+    events::{create_mode::CreateModeEvent, edit_mode::EditModeEvent},
+    prelude::PluginSettings,
+};
 
 pub fn handle_drawing_mode_transition(
     keyboard_input: Res<Input<KeyCode>>,
+    settings: Res<PluginSettings>,
     mut edit_mode_event: EventWriter<EditModeEvent>,
     mut create_mode_event: EventWriter<CreateModeEvent>,
 ) {
-    if keyboard_input.just_pressed(KeyCode::Key1) {
+    let PluginSettings { key_binds, .. } = *settings;
+    if keyboard_input.just_pressed(key_binds.edit_mode_switch_key) {
         info!("Change to edit mode...");
         create_mode_event.send(CreateModeEvent::EditModeSwitch);
-    } else if keyboard_input.just_pressed(KeyCode::Key2) {
+    } else if keyboard_input.just_pressed(key_binds.create_mode_switch_key) {
         info!("Change to create mode...");
         edit_mode_event.send(EditModeEvent::CreateModeSwitch);
     }
